0a17f500507efe430d209939cef9d7a9b0d1eebe
Rayne Flores <rayne.flores@digital-cupcake.com>
2021-07-08 16:18:59 -0400

    Cambio en Database

--- database.php
+++ database.php
@@ -1,15 +1,8 @@
 <?php
-	/*Parametros Rayne*/
 	$server ='localhost';
 	$username = 'root';
 	$password = '';
 	$database ='projecto_alex_database';
-
-	/*Parametros Alexis*/
-	/*$server ='localhost:3310';
-	$username = 'root';
-	$password = '';
-	$database ='projecto_alex_database';*/
 
 	try {
 		$conn = new PDO("mysql:host=$server;dbname=$database;",$username, $password);
